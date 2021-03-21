d1db122a5e7c730d538e34d1676b6e53bafb32a4
Erdonov <9682228i@gmail.com>
2021-03-21 06:31:18 +0500

    solved Question8.java

---
+++ exercises/src/assignments/assignment1/Question8.java
@@ -0,0 +1,26 @@
+package assignments.assignment1;
+
+public class Question8 {
+	
+	public static void main(String[] args) {
+		
+	/*Question8. Write a program that will print your initials to
+	 			 standard output in letters that are nine lines tall.
+				 Each big letter should be made up of a bunch of *'s. 
+				 For example, if your initials were "DJE",
+				 then the output would look something like: */
+		
+		System.out.println("My initials are 'E' 'I' 'U'\n\n" );
+		System.out.println("********     ******      **      **");
+		System.out.println("**             **        **      **");
+		System.out.println("**             **        **      **");
+		System.out.println("********       **        **      **");
+		System.out.println("**             **        **      **");
+		System.out.println("**             **         **    **");
+		System.out.println("********     ******        ******");
+		
+		
+		
+		
+	}
+}
