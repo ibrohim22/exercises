cc95273ebf17b8178d5199ef1d3b583908340434
Erdonov <9682228i@gmail.com>
2021-03-21 06:06:31 +0500

    solved Question6.java

---
+++ exercises/src/assignments/assignment1/Question6.java
@@ -0,0 +1,24 @@
+package assignments.assignment1;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		
+		//Question6. Write Java statements to print the following
+		//			 Empire State building shape into the console:
+		
+		System.out.println("   ^   ");
+		System.out.println("  / \\");
+		System.out.println(" /   \\");
+		System.out.println("/     \\");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("-------");
+	}
+
+}
