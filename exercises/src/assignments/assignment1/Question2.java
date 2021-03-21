bec6681d99f8f02d8a718d9090f08de49a825d2b
Erdonov <9682228i@gmail.com>
2021-03-21 05:41:31 +0500

    created Question2.java

---
+++ exercises/src/assignments/assignment1/Question2.java
@@ -0,0 +1,16 @@
+package assignments.assignment1;
+
+public class Question2 {
+	public static void main(String[] args) {
+		
+	/*Question2.  Suppose your name was Alan Turing.
+	 * Write a main method and a statement that would print your last name,
+	 *  followed by a comma, followed by a space and your first name
+	 */
+		System.out.println("Turing, Alan");
+		
+		
+		
+	}
+
+}
