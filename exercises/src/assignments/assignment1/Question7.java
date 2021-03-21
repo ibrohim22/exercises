12aa5d343ea15e6987a19ef6ace025e6eb94183d
Erdonov <9682228i@gmail.com>
2021-03-21 06:15:06 +0500

    solved Question7.java

---
+++ exercises/src/assignments/assignment1/Question7.java
@@ -0,0 +1,18 @@
+package assignments.assignment1;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		//Write Java statements to print the following
+		//I am from "Germany".
+		//How is your day going on, 'good' or 'bad' ?
+		
+		System.out.println(" /\\  /\\");
+		System.out.println("/__\\/__\\");
+		System.out.println("I am from \"Germany\".");
+		System.out.println("How is your day going on, 'good' or 'bad' ?");
+		
+	}
+
+}
