8dc54f7d52aa97259684ebf8095c89cb226a0fe8
Erdonov <9682228i@gmail.com>
2021-03-21 05:50:46 +0500

    solved Question4.java

---
+++ exercises/src/assignments/assignment1/Question4.java
@@ -0,0 +1,15 @@
+package assignments.assignment1;
+
+public class Question4 {
+	/*
+	 * Rearrange the following code so that it forms a correct program that prints
+	 * out the letter Q } public static void main(String[] arg) { }
+	 * System.out.println("Q"); public class Main {
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println("Q");
+
+	}
+}
