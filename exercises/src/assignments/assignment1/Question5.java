bfe5545b19f9a8b00ffc6c5a9c6a9429a5136c7a
Erdonov <9682228i@gmail.com>
2021-03-21 05:59:14 +0500

    solved Question5.java

---
+++ exercises/src/assignments/assignment1/Question5.java
@@ -0,0 +1,20 @@
+package assignments.assignment1;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+
+		//Question5. Write Java statements to print the following  into the console
+		
+		System.out.println("* * * * * * *");
+		System.out.println("* *       * *");
+		System.out.println("*   *   *   *");
+		System.out.println("*     *     *");
+		System.out.println("*   *   *   *");
+		System.out.println("* *       * *");
+		System.out.println("* * * * * * *");
+		
+		
+	}
+
+}
