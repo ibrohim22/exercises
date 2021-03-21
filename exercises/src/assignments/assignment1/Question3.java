e9c8d01ffc9c0eb510d53a98a64adef543483131
Erdonov <9682228i@gmail.com>
2021-03-21 05:45:37 +0500

    solved Question3.java

---
+++ exercises/src/assignments/assignment1/Question3.java
@@ -0,0 +1,22 @@
+package assignments.assignment1;
+
+public class Question3 {
+
+	public static void main (String[] args) {
+		
+	//Question3. Write multiple print statements that will print from 1 to 10 in separate lines.	
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
+		System.out.println(8);
+		System.out.println(9);
+		System.out.println("10");
+		
+		
+	}
+}
