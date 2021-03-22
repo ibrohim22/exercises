631752fb06e7bb89f1a1b254d0f1fa11f795523b
Erdonov <9682228i@gmail.com>
2021-03-22 23:14:10 +0500

    solved Question5.java

---
+++ exercises/src/assignments/assignment2/Question5.java
@@ -0,0 +1,19 @@
+package assignments.assignment2;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		
+		/* Declare and initialize two variables called first and second. 
+		 * Write a single statement that will print the message 
+		 * "first is " followed by the value of first, and then space, 
+		 * followed by "second = ", followed by the value of the second.
+		 * Ex: first is 55 second = 123 */
+		
+		int first=77, second=78;
+		System.out.println("First is " + first+ " second = " +second);
+		
+
+	}
+
+}
