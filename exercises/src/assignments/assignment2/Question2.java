44c41b8fcab30e1d5aec43097184163535892e72
Erdonov <9682228i@gmail.com>
2021-03-22 21:37:45 +0500

    solved Question2.java

---
+++ exercises/src/assignments/assignment2/Question2.java
@@ -0,0 +1,16 @@
+package assignments.assignment2;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		
+		/*Declare and initialize a String variable called message,
+		 *and write a statement to display its value to console.
+		 */
+
+		String message="Hello World";
+		System.out.println(message);
+		
+	}
+
+}
