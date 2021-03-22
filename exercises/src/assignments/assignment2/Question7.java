570aed4c49e5b17e55e229491cb4aff5d794a1e3
Erdonov <9682228i@gmail.com>
2021-03-23 03:24:24 +0500

    solved Question7.java

---
+++ exercises/src/assignments/assignment2/Question7.java
@@ -0,0 +1,19 @@
+package assignments.assignment2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		/*
+		 * Declare and initialize an integer variable i and a floating-point variable f.
+		 * Write a statement that writes both of their values to console in the
+		 * following format: i=value-of-i f=value-of-f Ex:Assume i = 9 and f = 33.4The
+		 * output should be:i=9 f=33.4
+		 */
+
+		int i=9; float f=33.4f;
+		
+		System.out.println("i="+i+" f="+f);
+		
+	}
+
+}
