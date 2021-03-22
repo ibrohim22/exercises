b2e5d621c7e48eff668c1e6870f98506eee75b24
Erdonov <9682228i@gmail.com>
2021-03-22 21:44:53 +0500

    solved Question3.java

---
+++ exercises/src/assignments/assignment2/Question3.java
@@ -0,0 +1,19 @@
+package assignments.assignment2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+		/*Declare two variables. One of them integer called num and 
+		 *the other one is double called cost. Print both values 
+		 *(num first, then cost), separated by a space on a single line.
+		 */
+		
+		int num=7;
+		double cost=100;
+		
+		System.out.println(num+ " "+cost);
+
+	}
+
+}
