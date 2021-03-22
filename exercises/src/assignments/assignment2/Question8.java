69e01b6c25ec34b66f9e3f40cd39aa231f8128b8
Erdonov <9682228i@gmail.com>
2021-03-23 03:32:44 +0500

    solved Question8.java

---
+++ exercises/src/assignments/assignment2/Question8.java
@@ -0,0 +1,20 @@
+package assignments.assignment2;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		/*
+		 * Declare and assign 2 String variables. They will hold different programming
+		 * language names. Using those variables print the message exactly like below.
+		 * Ex: Assume lan1 = "Java" and lan2 = "SQL" You need to print exactly: I will
+		 * learn "Java" and "SQL" at CybertekSchool.
+		 */
+		
+		String lan1="Java",lan2="SQL";
+		
+		System.out.println("I will learn \""+lan1+ "\" and \""+lan2+ "\" at Cybertekschool.");
+		
+		
+	}
+
+}
