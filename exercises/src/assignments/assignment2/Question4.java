b6ec4e8169c3db9c8922ead3e04faa4287be5d42
Erdonov <9682228i@gmail.com>
2021-03-22 23:04:52 +0500

    solved Question4.java

---
+++ exercises/src/assignments/assignment2/Question4.java
@@ -0,0 +1,21 @@
+package assignments.assignment2;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		
+		/*Assume that word is a String variable and already has a value.
+		 * Write a statement to display the message "Today's Word-Of-The-Day is: 
+		 * " followed by the value of the word.
+		 * The message and the value of word should appear together, on a single line.
+		 * Ex: Today's Word-Of-The-Day is: Java
+		 */
+		String word="Yasmin";
+		
+		System.out.println("Today's Word-Of-The-Day is: "+word);
+	
+		
+		
+	}
+
+}
