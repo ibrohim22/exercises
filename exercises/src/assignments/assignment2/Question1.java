b71c134b586986c15683f92c192c10d0b2ccb4e9
Erdonov <9682228i@gmail.com>
2021-03-22 21:16:39 +0500

    solved Question1.java

---
+++ exercises/src/assignments/assignment2/Question1.java
@@ -0,0 +1,16 @@
+package assignments.assignment2;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+		/*Declare and initialize an integer variable called count,
+		 *and write a statement that displays the value of count
+		 * on the console*/
+		int count;
+		count=10;
+		System.out.println("The value of count is "+count);
+		
+	}
+
+}
