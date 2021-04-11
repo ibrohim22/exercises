c5ac21359fabd6b0c8ecab9edce51fcdb5fbe396
Erdonov <9682228i@gmail.com>
2021-04-11 21:11:48 +0500

    assignment3_q3Solved

---
+++ exercises/src/assignments/assignment3/Question_3.java
@@ -0,0 +1,25 @@
+package assignments.assignment3;
+
+public class Question_3 {
+
+	public static void main(String[] args) {
+
+		/* Write a Java program that displays the area of a rectangle with a width of
+		 * 4.5 and a height of 7.9 using the following formula:
+		 * 
+		 * area = width * height
+		 */
+
+		
+		double width=4.5;
+		double height=7.9;
+		
+		double area = width* height;
+		
+		
+		
+		System.out.println("The are of a given rectangle is "+ area);
+		
+	}
+
+}
