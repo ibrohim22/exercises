ee2329aec01859256c3bb5e93b0b353e6bbc7a67
Erdonov <9682228i@gmail.com>
2021-04-11 21:01:05 +0500

    assignment3_q1Solved

---
+++ exercises/src/assignments/assignment3/Question_1.java
@@ -0,0 +1,24 @@
+package assignments.assignment3;
+
+public class Question_1 {
+
+	public static void main(String[] args) {
+
+//
+//		Write a program to convert gallons into Liters and display it.
+//
+//      Sample output:
+//
+//      15 gallon is 56.7812 liter
+		
+		int gallon=15;
+		double liter=gallon*3.785411784;
+		
+		System.out.println(gallon+ " gallon is "+ liter + " liters");
+		
+		
+		
+
+	}
+
+}
