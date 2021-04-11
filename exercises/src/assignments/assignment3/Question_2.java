d1f2ba99da809f4fddb1ca7fb126e72612d2b3af
Erdonov <9682228i@gmail.com>
2021-04-11 21:01:41 +0500

    assingment3_q2Solved

---
+++ exercises/src/assignments/assignment3/Question_2.java
@@ -0,0 +1,23 @@
+package assignments.assignment3;
+
+public class Question_2 {
+
+	public static void main(String[] args) {
+
+//		Write a program - > Declare variables for your name and birth year,
+//		and the program will display:
+//
+//		" Hello, Ozzy! Based on your input, your age is 15 :) "
+
+		String name="Ozzy";
+		int birthYear=2006;
+		
+		int actualYear=2021;
+		int age=actualYear-birthYear;
+		
+		System.out.println("\" Hello, " + name + "! Based on your input, your age is " + age + " :) \"");
+		
+
+	}
+
+}
